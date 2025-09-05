f4eb1da2ed7a0d36699bd77570b159f4bba6d532
Denis Stepushchik <denis.stepushchik@nove8.com>
2025-09-05 09:09:19 +0300

    add doc

--- preferences/lib/src/util/object_util.dart
+++ preferences/lib/src/util/object_util.dart
@@ -1,10 +1,14 @@
 import 'package:async/async.dart';
 import 'package:common_result/common_result.dart';
 
+/// Extension methods for nullable objects to provide additional utility functionality.
 extension NullableObjectExtensions<T> on T {
+  /// Wraps this value in a [Future] that completes immediately with the value.
   Future<T> toFuture() => Future<T>.value(this);
 }
 
+/// Maps a value provider function to a [Result], catching any exceptions
+/// and converting them to [Failure] results.
 Result<T> mapToResult<T>({
   required T Function() valueProvider,
   required Failure Function(Object error) failureProvider,
