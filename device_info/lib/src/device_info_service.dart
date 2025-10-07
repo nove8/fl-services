c9ae8913a2d1e6e32172505890d2311dd545d6ca
Alexander Ilyin <alexander.ilyin@nove8.com>
2025-10-07 10:35:00 +0300

    STL-244: [device_info] Add initial device info service with device_info_plus package implementation

---
+++ device_info/lib/src/device_info_service.dart
@@ -0,0 +1,8 @@
+import 'package:async/async.dart';
+
+/// A service interface for retrieving device-specific information.
+abstract interface class DeviceInfoService {
+  /// An alphanumeric string that uniquely identifies a device to the app’s vendor.
+  /// Available on iOS platform only. On other platforms will be [UnsupportedPlatformFailure].
+  Future<Result<String?>> getIdentifierForVendor();
+}
---
+++ device_info/lib/src/failure/device_info_failure.dart
@@ -0,0 +1,24 @@
+import 'package:common_result/common_result.dart';
+
+/// Base class for device info failures.
+sealed class DeviceInfoFailure implements Failure {}
+
+/// Failure that occurs when retrieving iOS device information from the platform.
+final class GetIosDeviceInfoFailure implements DeviceInfoFailure {
+  /// Creates a [GetIosDeviceInfoFailure] with the underlying error.
+  const GetIosDeviceInfoFailure(this.error);
+
+  /// The underlying error object from the platform API call.
+  final Object error;
+
+  @override
+  String toString() {
+    return 'GetDeviceInfoFailure{error: $error}';
+  }
+}
+
+/// Failure that occurs when retrieving device information from the platform isn't possible.
+final class UnsupportedPlatformFailure implements DeviceInfoFailure {
+  /// Creates a [UnsupportedPlatformFailure].
+  const UnsupportedPlatformFailure();
+}
