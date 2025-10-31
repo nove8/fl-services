cd997ebe7538be891f0d889f013791df65e6601b
Alexander Ilyin <alexander.ilyin@nove8.com>
2025-10-31 10:07:16 +0300

    STL-244: [haptic] Fix docs

--- haptic/lib/src/haptic_service.dart
+++ haptic/lib/src/haptic_service.dart
@@ -3,7 +3,6 @@ import 'package:haptic_service/src/entity/haptic_impact.dart';
 
 /// A service interface for performing haptic on the device.
 abstract interface class HapticService {
-  /// An alphanumeric string that uniquely identifies a device to the app’s vendor.
-  /// Available on iOS platform only. On other platforms will be [UnsupportedPlatformFailure].
+  /// Performing haptic impact.
   Future<Result<void>> performHapticImpact(HapticImpact hapticImpact);
 }
