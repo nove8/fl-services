bada6857be57f42a88e293ba4f29cfa9ec8a119a
Alexander Ilyin <alexander.ilyin@nove8.com>
2025-10-31 16:54:44 +0300

    STL-244: [device_info] Update device_info_plus version to 12.2.0. Add getOsVersion method to service

--- device_info/lib/src/device_info_service.dart
+++ device_info/lib/src/device_info_service.dart
@@ -5,4 +5,7 @@ abstract interface class DeviceInfoService {
   /// An alphanumeric string that uniquely identifies a device to the app’s vendor.
   /// Available on iOS platform only. On other platforms will be [UnsupportedPlatformFailure].
   Future<Result<String?>> getIdentifierForVendor();
+
+  /// The current operating system version.
+  Future<Result<String>> getOsVersion();
 }
