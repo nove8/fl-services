f56d5b74ee20f360eb23080771586789802ed3d1
Denis Stepushchik <denis.stepushchik@nove8.com>
2026-01-12 11:40:49 +0300

    BOO-0000: Fix check lint script

--- check_lint.sh
+++ check_lint.sh
@@ -10,7 +10,7 @@ dirs=$(find "$script_dir" -mindepth 1 -maxdepth 1 -type d \
   -print0 | xargs -0 -n1 basename)
 printf "dir to analyze with dart_code_metrics: %s\n" $dirs
 
-dcm analyze $dirs --fatal-style --fatal-performance --fatal-warnings
+dcm analyze $dirs --fatal-style --fatal-warnings
 dcm check-dependencies $dirs --ignored-packages=common_package
 
 printf "Done!\n\n"
