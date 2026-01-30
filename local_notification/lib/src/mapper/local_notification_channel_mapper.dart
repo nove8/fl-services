abc5a867ccdc407a78d78da9f3e34f4b4eefae9c
Ivan Starchykov <ivan.starchykov@nove8.com>
2026-01-30 12:31:52 +0200

    BOO-8149: [local_notification] Add custom sound support to notification channels

--- local_notification/lib/src/mapper/local_notification_channel_mapper.dart
+++ local_notification/lib/src/mapper/local_notification_channel_mapper.dart
@@ -1,13 +1,21 @@
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:local_notification_service/src/entity/local_notification_channel.dart';
+import 'package:local_notification_service/src/mapper/local_notification_mappers.dart';
 
 /// Mapper for converting [LocalNotificationChannel] to [AndroidNotificationChannel].
 class LocalNotificationChannelToAndroidNotificationChannelMapper {
   /// Creates a [LocalNotificationChannelToAndroidNotificationChannelMapper].
   const LocalNotificationChannelToAndroidNotificationChannelMapper();
 
+  static const LocalNotificationSoundToAndroidSoundMapper _soundToAndroidSoundMapper =
+      LocalNotificationSoundToAndroidSoundMapper();
+
   /// Transforms [LocalNotificationChannel] to [AndroidNotificationChannel].
   AndroidNotificationChannel transform({required LocalNotificationChannel channel}) {
-    return AndroidNotificationChannel(channel.id, channel.name);
+    return AndroidNotificationChannel(
+      channel.id,
+      channel.name,
+      sound: _soundToAndroidSoundMapper.transform(channel.soundResourceName),
+    );
   }
 }
