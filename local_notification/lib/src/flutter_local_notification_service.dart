ffb2a53ba308e34cffefada4007443413fed6386
Ivan Starchykov <ivan.starchykov@nove8.com>
2026-01-30 13:05:07 +0200

    BOO-8149: [local_notification] Add custom sound support to notification channels

--- local_notification/lib/src/flutter_local_notification_service.dart
+++ local_notification/lib/src/flutter_local_notification_service.dart
@@ -48,8 +48,6 @@ final class FlutterLocalNotificationService implements LocalNotificationService 
       const LocalNotificationImportanceToAndroidImportanceMapper();
   final LocalNotificationPriorityToAndroidPriorityMapper _priorityToAndroidPriorityMapper =
       const LocalNotificationPriorityToAndroidPriorityMapper();
-  final LocalNotificationChannelSoundToAndroidSoundMapper _channelSoundToAndroidSoundMapper =
-      const LocalNotificationChannelSoundToAndroidSoundMapper();
   final LocalNotificationFilePathToDarwinNotificationAttachmentMapper _filePathToAttachmentMapper =
       const LocalNotificationFilePathToDarwinNotificationAttachmentMapper();
   final LocalNotificationScheduleModeToAndroidScheduleModeMapper _scheduleModeToAndroidScheduleModeMapper =
@@ -223,7 +221,6 @@ final class FlutterLocalNotificationService implements LocalNotificationService 
         importance: _importanceToAndroidImportanceMapper.transform(androidDetails.importance),
         priority: _priorityToAndroidPriorityMapper.transform(androidDetails.priority),
         playSound: androidDetails.shouldPlaySound,
-        sound: _channelSoundToAndroidSoundMapper.transform(channel.soundResourceName),
         enableVibration: androidDetails.shouldEnableVibration,
         vibrationPattern: androidDetails.vibrationPattern,
         channelShowBadge: androidDetails.shouldShowBadge,
