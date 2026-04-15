ad41e1bec62a1762d028cec09a4bf6e4243149e6
Denis Stepushchik <denis.stepushchik@nove8.com>
2026-04-15 16:42:46 +0300

    BOO-8414: [remote_config] Return null for missing parameters
    BREAKING CHANGE: all public API methods return Result<T?> instead of Result<T>.
    BREAKING CHANGE: remove `MissingRemoteConfigParameterForKeyFailure`
    chore: bump `firebase_remote_config` to `^6.4.0`
    chore: bump `common_package` version to `4.2.0`
    chore: bump version to `4.0.0`
    chore: add `.flutter-plugins-dependencies` to `.gitignore`

--- remote_config/lib/src/remote_config_service.dart
+++ remote_config/lib/src/remote_config_service.dart
@@ -6,76 +6,89 @@ import 'package:remote_config_service/src/failure/remote_config_failure.dart';
 /// An interface for a service that provides remote configuration parameters.
 abstract interface class RemoteConfigService {
   /// Awaits config initialization and gets [String] parameter value for selected [parameterKey].
-  Future<Result<String>> getStringParameterValue({required String parameterKey});
+  /// Returns null if the parameter with [parameterKey] is not configured in remote config.
+  Future<Result<String?>> getStringParameterValue({required String parameterKey});
 
   /// Awaits config initialization and gets [int] parameter value for selected [parameterKey].
-  Future<Result<int>> getIntParameterValue({required String parameterKey});
+  /// Returns null if the parameter with [parameterKey] is not configured in remote config.
+  Future<Result<int?>> getIntParameterValue({required String parameterKey});
 
   /// Awaits config initialization and gets [double] parameter value for selected [parameterKey].
-  Future<Result<double>> getDoubleParameterValue({required String parameterKey});
+  /// Returns null if the parameter with [parameterKey] is not configured in remote config.
+  Future<Result<double?>> getDoubleParameterValue({required String parameterKey});
 
   /// Awaits config initialization and gets [bool] parameter value for selected [parameterKey].
-  Future<Result<bool>> getBoolParameterValue({required String parameterKey});
+  /// Returns null if the parameter with [parameterKey] is not configured in remote config.
+  Future<Result<bool?>> getBoolParameterValue({required String parameterKey});
 
   /// Awaits config initialization and gets parameter value as List of [T] values
   /// for selected [parameterKey].
-  Future<Result<List<T>>> getListParameterValue<T>({required String parameterKey});
+  /// Returns null if the parameter with [parameterKey] is not configured in remote config.
+  Future<Result<List<T>?>> getListParameterValue<T>({required String parameterKey});
 
   /// Awaits config initialization and gets parameter value as Set of [T] values
   /// for selected [parameterKey].
-  Future<Result<Set<T>>> getSetParameterValue<T>({required String parameterKey});
+  /// Returns null if the parameter with [parameterKey] is not configured in remote config.
+  Future<Result<Set<T>?>> getSetParameterValue<T>({required String parameterKey});
 
   /// Awaits config initialization and gets complex feature parameter value for selected [parameterKey].
+  /// Returns null if the parameter with [parameterKey] is not configured in remote config.
   /// On any thrown error from [featureFactory] will be returned [RemoteConfigFeatureParameterParsingFailure].
-  Future<Result<FeatureT>> getFeatureParameterValue<FeatureT extends Object>({
+  Future<Result<FeatureT?>> getFeatureParameterValue<FeatureT extends Object>({
     required String parameterKey,
     required FeatureT Function(Map<String, Object?> json) featureFactory,
   });
 
   /// Awaits config initialization and gets [String] labels segmented parameter value
   /// for selected [parameterKey].
+  /// Returns null if the parameter with [parameterKey] is not configured in remote config.
   /// [userLabels] is a map of user labels where key is the label key and value is the label value.
-  Future<Result<String>> getStringLabelsSegmentedParameterValue({
+  Future<Result<String?>> getStringLabelsSegmentedParameterValue({
     required String parameterKey,
     required FutureOr<Map<String, String>> userLabels,
   });
 
   /// Awaits config initialization and gets [int] labels segmented parameter value
   /// for selected [parameterKey].
+  /// Returns null if the parameter with [parameterKey] is not configured in remote config.
   /// [userLabels] is a map of user labels where key is the label key and value is the label value.
-  Future<Result<int>> getIntLabelsSegmentedParameterValue({
+  Future<Result<int?>> getIntLabelsSegmentedParameterValue({
     required String parameterKey,
     required FutureOr<Map<String, String>> userLabels,
   });
 
   /// Awaits config initialization and gets [double] labels segmented parameter value
   /// for selected [parameterKey].
+  /// Returns null if the parameter with [parameterKey] is not configured in remote config.
   /// [userLabels] is a map of user labels where key is the label key and value is the label value.
-  Future<Result<double>> getDoubleLabelsSegmentedParameterValue({
+  Future<Result<double?>> getDoubleLabelsSegmentedParameterValue({
     required String parameterKey,
     required FutureOr<Map<String, String>> userLabels,
   });
 
   /// Awaits config initialization and gets [bool] labels segmented parameter value
   /// for selected [parameterKey].
+  /// Returns null if the parameter with [parameterKey] is not configured in remote config.
   /// [userLabels] is a map of user labels where key is the label key and value is the label value.
-  Future<Result<bool>> getBoolLabelsSegmentedParameterValue({
+  Future<Result<bool?>> getBoolLabelsSegmentedParameterValue({
     required String parameterKey,
     required FutureOr<Map<String, String>> userLabels,
   });
 
   /// Awaits config initialization and gets parameter value as List of [T] values
   /// for selected [parameterKey].
+  /// Returns null if the parameter with [parameterKey] is not configured in remote config.
   /// [userLabels] is a map of user labels where key is the label key and value is the label value.
-  Future<Result<List<T>>> getListLabelsSegmentedParameterValue<T>({
+  Future<Result<List<T>?>> getListLabelsSegmentedParameterValue<T>({
     required String parameterKey,
     required FutureOr<Map<String, String>> userLabels,
   });
 
   /// Awaits config initialization and gets parameter value as Set of [T] values
   /// for selected [parameterKey].
+  /// Returns null if the parameter with [parameterKey] is not configured in remote config.
   /// [userLabels] is a map of user labels where key is the label key and value is the label value.
-  Future<Result<Set<T>>> getSetLabelsSegmentedParameterValue<T>({
+  Future<Result<Set<T>?>> getSetLabelsSegmentedParameterValue<T>({
     required String parameterKey,
     required FutureOr<Map<String, String>> userLabels,
   });
