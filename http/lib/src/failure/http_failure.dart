4ee7cd304469c41df4061a44c45eb6e5fcd0724d
Denis Stepushchik <denis.stepushchik@nove8.com>
2026-05-26 13:34:47 +0300

    BOO-8659: [http] Add optional http request timeout
    feat: Add optional http request timeout

--- http/lib/src/failure/http_failure.dart
+++ http/lib/src/failure/http_failure.dart
@@ -88,6 +88,12 @@ final class HttpRequestFailure implements HttpFailure {
   }
 }
 
+/// Failure that occurs when an HTTP request exceeds the configured timeout.
+final class HttpRequestTimeoutFailure extends HttpRequestFailure {
+  /// Creates a [HttpRequestTimeoutFailure].
+  const HttpRequestTimeoutFailure(super.error);
+}
+
 /// Failure that occurs when there is no network connection available.
 final class NoNetworkConnectionFailure implements HttpFailure {
   /// Creates a [NoNetworkConnectionFailure].
